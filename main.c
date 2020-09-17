bfedde0bed870c5f8a7d6e9e997c4315eb70315f
soeun5124 <71041198+soeun5124@users.noreply.github.com>
2020-09-17 16:45:07 +0900

    submisson04

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 
 int main(int argc, char *argv[]) {
 	
-	char c;
-
-	printf("enter an alphabet :");
-	scanf("%c", &c);
+	int a;
+	float b;
+	printf("input two integers :");
+	scanf("%d %f", &a, &b);
 	
-    printf("input alphabet is %c (%d), and the next one is %c (%d)\n", c, c, c+1, c+1 );
-    
+	printf("result is %f\n", a/b);
+	
+	
 	return 0;
 }
