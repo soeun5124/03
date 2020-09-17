736ef6f106c8d0d7ea8c26d9973e9b17aaaecbcf
soeun5124 <71041198+soeun5124@users.noreply.github.com>
2020-09-17 16:27:23 +0900

    submisson03

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
 	char c;
-	int i;
+
+	printf("enter an alphabet :");
+	scanf("%c", &c);
 	
-	printf("input a number :");
-	scanf("%c",&c);
-	
-	i = c - '0' ;
-	printf("The input number is %i\n",i);
+    printf("input alphabet is %c (%d), and the next one is %c (%d)\n", c, c, c+1, c+1 );
+    
 	return 0;
 }
